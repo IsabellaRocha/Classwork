12d9ff617b011ff73de618d35614dcd17bf6e826
Isabella Rocha <irocha00@stuy.edu>
2019-03-11 00:25:18 -0400

    Partition finally works

--- Classwork.java
+++ Classwork.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Classwork {
   public static ArrayList<String> makeAllWords(int k, int maxLetter) {
@@ -19,30 +19,37 @@ public class Classwork {
   public static int partition (int[] data, int start, int end) {
     start = 1;
     end = data.length - 1;
-    int rando = (int) Math.random() * data.length;
+    int rando = (int) (Math.random() * (data.length));
     int pivot = data[rando];
     data[rando] = data[0];
     data[0] = pivot;
     while (start < end) {
-      int s = data[start];
-      int e = data[end];
-      if (s > pivot || e < pivot) {
+      if (data[start] > pivot) {
+        while (data[end] > pivot && end > start) {
+          end--;
+        }
+        int s = data[start];
+        int e = data[end];
         data[start] = e;
         data[end] = s;
-        start++;
-        end--;
       }
+      start++;
     }
     if (data[start] < pivot) {
       data[0] = data[start];
       data[start] = pivot;
-      return start;
     }
-    else {
+    else if (data[start - 1] < pivot){
       data[0] = data[start - 1];
       data[start - 1] = pivot;
-      return start - 1;
+      start -= 1;
     }
+    else {
+      data[0] = data[start - 2];
+      data[start - 2] = pivot;
+      start -= 2;
+    }
+    return start;
   }
 
   public static void insertionSort(int[] ary) {
@@ -59,8 +66,10 @@ public class Classwork {
   }
 
   public static void main(String[] args) {
-    System.out.println(makeAllWords(2, 3));
-    System.out.println(makeAllWords(2, 20));
-    System.out.println(makeAllWords(5, 7));
+  //  System.out.println(makeAllWords(2, 3));
+  //  System.out.println(makeAllWords(2, 20));
+  //  System.out.println(makeAllWords(5, 7));
+    int[] ary = {17,61,67,47,93,12,20,4,44,78};
+    System.out.println(partition(ary, 0, ary.length));
   }
 }
