a7f6f4e9d9814fcc2186c147e61d039a8949bdcb
Isabella Rocha <irocha00@stuy.edu>
2019-03-10 22:29:25 -0400

    Wrote first draft of partition

--- Classwork.java
+++ Classwork.java
@@ -16,6 +16,55 @@ public class Classwork {
       }
     }
   }
+  public static int partition (int[] data, int start, int end) {
+    start = 1;
+    end = data.length;
+    int rando = (int) Math.random() * data.length;
+    int pivot = data[rando];
+    int temp = data[start];
+    data[0] = pivot;
+    data[rando] = temp;
+    while (start < end) {
+      if (data[start] > pivot) {
+        int cur = data[start];
+        int swap = data[end];
+        data[start] = swap;
+        data[end] = cur;
+        start++;
+      }
+      if (data[end] < pivot) {
+        int cur = data[end];
+        int swap = data[start];
+        data[start] = cur;
+        data[end] = swap;
+        end++;
+      }
+    }
+    if (data[start] < pivot) {
+      data[0] = data[start];
+      data[start] = pivot;
+      return start;
+    }
+    else {
+      data[0] = data[start - 1];
+      data[start - 1] = pivot;
+      return start - 1;
+    }
+  }
+
+  public static void insertionSort(int[] ary) {
+    for (int idx = 1; idx < ary.length; idx++) {
+      int current = ary[idx]; // Storing value for later to move
+      int curIdx = idx - 1;
+      while (curIdx >= 0 && ary[curIdx] > current) {
+        ary[curIdx + 1] = ary[curIdx]; // Shifting values over
+        curIdx--;
+      }
+      ary[curIdx + 1] = current;
+    //  System.out.println(Arrays.toString(ary));
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println(makeAllWords(2, 3));
     System.out.println(makeAllWords(2, 20));
